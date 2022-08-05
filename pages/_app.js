4d0a6eab78960fced582362f58758f68cb32312f
Muhammad Zubair <2020cs601@student.uet.edu.pk>
2022-08-05 16:07:42 +0500

    Initial Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="manifest" href="%PUBLIC_URL%/manifest.json"/>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;500&display=swap" rel="stylesheet" />
+      </Head>
+
+      
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
